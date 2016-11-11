673338fb34c826869dc7c9991ab276def473fb3b
LeonardoRk <leodegolim@yahoo.com.br>
2016-11-11 15:54:56 -0200

    folder correct to start programming

---
+++ EPICO1/src/model/Person.java
@@ -0,0 +1,19 @@
+package model;
+
+
+public class Person {
+	String name;
+	Person(String name){
+		setName(name);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
