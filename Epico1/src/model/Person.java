788b49f131b878d06719a9163ce8f8631d8ba585
MrVictor42 <guin-hu@hotmail.com>
2016-11-11 16:24:43 -0200

    Finish model Person

--- Epico1/src/model/Person.java
+++ Epico1/src/model/Person.java
@@ -1,5 +1,44 @@
 package model;
 
-public class Person {
+import java.util.Date;
 
+public abstract class Person {
+	
+	private String name;
+	private String sex;
+	private Date dataOfBirth;
+	
+	public Person() {
+		
+	}
+
+	public Person (String name,String sex,Date dateOfBirth) {
+		setName(name);
+		setSex(sex);
+		setDataOfBirth(dateOfBirth);
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public Date getDataOfBirth() {
+		return dataOfBirth;
+	}
+
+	public void setDataOfBirth(Date dataOfBirth) {
+		this.dataOfBirth = dataOfBirth;
+	}
 }
