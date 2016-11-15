53626139a7b72f53ef234d88218e7083cfe4a43f
MrVictor42 <guin-hu@hotmail.com>
2016-11-15 20:50:36 -0200

    Database

--- Epico1/src/resourses/database.sql
+++ Epico1/src/resourses/database.sql
@@ -1,25 +1,24 @@
-CREATE DATABASE if not exists cef;
+CREATE DATABASE IF NOT EXISTS cef;
 USE cef;
 
 CREATE TABLE  IF NOT EXISTS Student (
-idStudent INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(42) NOT NULL,
-dateOfBirth VARCHAR(10),
-letterClass VARCHAR(1) NOT NULL ,
-room VARCHAR(1) NOT NULL ,
-registration VARCHAR(10) NOT NULL,
-turn VARCHAR(1) NOT NULL,
-year VARCHAR(1) NOT NULL,
-modalityCourse VARCHAR(6) NOT NULL,
-scholarshio VARCHAR(1) NOT NULL
+	registration VARCHAR(10) PRIMARY KEY NOT NULL,
+	name VARCHAR(42) NOT NULL,
+	dateOfBirth VARCHAR(10),
+	letterClass VARCHAR(1) NOT NULL ,
+	room VARCHAR(1) NOT NULL ,
+	turn VARCHAR(1) NOT NULL,
+	year VARCHAR(1) NOT NULL,
+	modalityCourse VARCHAR(6) NOT NULL,
+	scholarshio VARCHAR(1) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Responsible (
-idResponsabile INT UNSIGNED PRIMARY KEY auto_increment,
-passoword VARCHAR(10) ,
-name VARCHAR(42),
-dateOfBirth VARCHAR(10),
-idStudent INT UNSIGNED,
-FOREIGN KEY (idStudent) REFERENCES Student(idStudent)
+	idResponsabile INT UNSIGNED PRIMARY KEY auto_increment,
+	passoword VARCHAR(10) ,
+	name VARCHAR(42),
+	dateOfBirth VARCHAR(10),
+	idStudent INT UNSIGNED,
+FOREIGN KEY (registration) REFERENCES Student(registration)
 )
 
